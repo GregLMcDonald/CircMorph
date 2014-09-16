78fe2e00b20eea067a732dc8d0b834fcd2ce01e7
Greg McDonald <greg@Nathan-McDonald.local>
2014-09-16 08:54:28 -0400

    swelling flower…cool!

--- CircMorpher/TMCMyScene.m
+++ CircMorpher/TMCMyScene.m
@@ -9,21 +9,91 @@
 #import "TMCMyScene.h"
 
 @implementation TMCMyScene
+{
+    int frame_count;
+    float flower_power;
+    float flower_power_change;
+}
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        frame_count = 0;
+        flower_power = 1.1;
+        flower_power_change = 0.05;
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        self.backgroundColor = [SKColor whiteColor];
         
-        [self addChild:myLabel];
+        SKShapeNode *circle = [[SKShapeNode alloc] init];
+        circle.name = @"flower";
+        
+        CGMutablePathRef circlePath = CGPathCreateMutable();
+        
+        
+        float radius = 20.0;
+        
+        float xi = 500 + radius * cosf((0) * M_PI / 5.0);
+        float yi = 500 + radius * sinf((0) * M_PI / 5.0);
+        CGPathMoveToPoint(circlePath, NULL, xi, yi);
+
+        NSMutableArray *points = [[NSMutableArray alloc]init];
+        
+        
+        
+
+        
+        
+        for (int i=1; i<=10; i++) {
+            
+            float xi = 500 + radius * cosf((i) * M_PI / 5.0);
+            float yi = 500 + radius * sinf((i) * M_PI / 5.0);
+            
+            float cr = flower_power;
+            float cx1 =  500 + cr * radius * cosf((i) * M_PI / 5.0);
+            float cy1 = 500 + cr * radius * sinf((i) * M_PI / 5.0);
+            float cx2 = 500 + cr * radius * cosf((i+1) * M_PI / 5.0);
+            float cy2 = 500 + cr * radius * sinf((i+1) * M_PI / 5.0);
+            
+            float xf = 500 + radius * cosf((i+1) * M_PI / 5.0);
+            float yf = 500 + radius * sinf((i+1) * M_PI / 5.0);
+            
+            
+            [points addObject:[[SKShapeNode alloc]init]];
+            
+            [(SKShapeNode *)points.lastObject setPath:CGPathCreateWithEllipseInRect(CGRectMake(xi-5, yi-5, 10, 10), NULL)];
+            [(SKShapeNode *)points.lastObject setFillColor:[SKColor blackColor]];
+            [(SKShapeNode *)points.lastObject setStrokeColor:[SKColor greenColor]];
+            
+            
+            
+            
+            CGPathAddCurveToPoint(circlePath, NULL,
+                                  cx1, cy1,
+                                  cx2, cy2,
+                                  xf, yf);
+        
+            
+        }
+        
+        circle.path = circlePath;
+        
+        CGPathRelease(circlePath);
+        
+        circle.strokeColor = [SKColor blackColor];
+        circle.lineWidth = .01        ;
+        circle.antialiased = YES;
+        circle.fillColor = [SKColor yellowColor];
+        
+        [self addChild:circle];
+        //for ( SKShapeNode *node in points){
+        //    [self addChild:node];
+        //}
+        
+        
+    
+        
     }
     return self;
 }
@@ -48,6 +118,55 @@
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+    if (frame_count == 1){
+        //redraw path
+        
+        SKNode *circle = [self childNodeWithName:@"flower"];
+        
+        if (flower_power > 3.0){
+            flower_power_change = -0.05;
+        }
+        if (flower_power < 1.2){
+            flower_power_change = 0.05;
+        }
+        
+        flower_power += flower_power_change;
+            
+        CGMutablePathRef circlePath = CGPathCreateMutable();
+        CGPathMoveToPoint(circlePath, NULL, 600, 500);
+        
+        float radius = 20.0;
+        
+        for (int i=0; i<10; i++) {
+            
+            //float xi = 500 + radius * cosf((i) * M_PI / 5.0);
+            //float yi = 500 + radius * sinf((i) * M_PI / 5.0);
+            
+            float cr = flower_power;
+            float cx1 =  500 + cr * radius * cosf((i) * M_PI / 5.0);
+            float cy1 = 500 + cr * radius * sinf((i) * M_PI / 5.0);
+            float cx2 = 500 + cr * radius * cosf((i+1) * M_PI / 5.0);
+            float cy2 = 500 + cr * radius * sinf((i+1) * M_PI / 5.0);
+            
+            float xf = 500 + radius * cosf((i+1) * M_PI / 5.0);
+            float yf = 500 + radius * sinf((i+1) * M_PI / 5.0);
+            
+            
+            CGPathAddCurveToPoint(circlePath, NULL,
+                                  cx1, cy1,
+                                  cx2, cy2,
+                                  xf, yf);
+            
+        }
+        
+        [(SKShapeNode *)circle setPath:circlePath];
+        
+        frame_count = 0;
+    }
+    
+    else {
+        frame_count++;
+    }
 }
 
 @end
